9339d1d3da63ad11bd65440387d4097fdc8ec2d7
Rafael Mavliukeev <49470479+rafael-mavliukeev@users.noreply.github.com>
2025-02-23 16:04:12 +0300

    ui finished

--- script.js
+++ script.js
@@ -42,3 +42,52 @@ function playGame () {
     console.log(playRound(humanChoice, computerChoice))
 }
 
+function choiceButtonClick (event){
+    let button = event.target
+    playerChoicePicture.setAttribute("src", `./pictures/${button.id}.png`)
+    playerChoice = button.id
+}
+
+function goButtonClick () {
+
+    choiceImageRotation()
+
+}
+
+async function choiceImageRotation () {
+    for (let i=0; i<10; i++){
+        await sleep(500 - 40 * i)
+        computerChoicePicture.setAttribute("src", `./pictures/${choices[i%2]}.png`)
+    } 
+    let computerChoice = getComputerChoice()
+    computerChoicePicture.setAttribute("src", `./pictures/${computerChoice}.png`)
+    let gameResult = playRound(playerChoice, computerChoice)
+    playerScore.textContent = Number(playerScore.textContent) + gameResult[0]
+    computerScore.textContent = Number(computerScore.textContent) + gameResult[1]
+
+    if (Number(playerScore.textContent)== 3) {
+        alert("Вы выиграли")
+    }
+    else if (Number(computerScore.textContent) == 3) {
+        alert("Компьютер выиграл")
+    }
+}
+
+async function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms))
+  }
+
+const playerScore = document.querySelector("#player-score")
+const computerScore = document.querySelector("#computer-score")
+const playerButtons = document.querySelector(".choice-buttons")
+const playerChoicePicture = document.querySelector("#player-choice-picture")
+const computerChoicePicture = document.querySelector("#computer-choice-picture")
+const goButton = document.querySelector("#go")
+const gameResult = document.querySelector("#result")
+
+let choices = ["rock", "paper", "scissors"]
+let playerChoice = "rock"
+
+playerButtons.addEventListener("click", choiceButtonClick)
+goButton.addEventListener("click", goButtonClick)
+
